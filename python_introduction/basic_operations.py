40f84dba12fd190a0696f5336658fa71e52f5149
Yaw Takyi Domfeh <ydot855@gmail.com>
2025-05-23 19:11:45 +0000

    Add directory and file

---
+++ python_introduction/basic_operations.py
@@ -0,0 +1,15 @@
+# variable naming
+number1 = 10
+number2 = 5
+
+# arithmetic operations
+
+sum_result = number1 + number2
+difference = number1 - number2
+product_result = number1 * number2
+
+# Results printing
+
+print(f"Addition of {number1} and {number2} is {sum_result}.")
+print(f"Subtraction of {number1} and {number2} is {difference}.")
+print(f"Multiplication of {number1} and {number2} is {product_result}.")
