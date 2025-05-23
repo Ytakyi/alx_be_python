657c921b6d61a5b1fd985b5dfd0aad8916b55af0
Yaw Takyi Domfeh <ydot855@gmail.com>
2025-05-23 19:25:03 +0000

    Add simple_interest.py

---
+++ python_introduction/simple_interest.py
@@ -0,0 +1,10 @@
+# naming of variables
+principal = 1000
+rate =  0.05
+time = 3
+
+# calculating simple interest
+
+interest = principal * rate * time
+
+print(f"The simple interest is: {interest}.")
